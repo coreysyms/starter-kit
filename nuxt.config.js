b9bdf802e2ca6d84a03ef8a39d4c9b3802b36048
Corey Snyder <corey@tangerineindustries.com>
2020-07-17 14:59:31 -0400

    2.0.0 changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,103 +1,89 @@
-require('dotenv').config()
-import pkg from './package'
-
 
 export default {
-  mode: 'universal',
-
-    /*
-  ** Headers of the page
+  /*
+  ** Nuxt rendering mode
+  ** See https://nuxtjs.org/api/configuration-mode
   */
- head: {
-  title: pkg.name,
-  meta: [
-    { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { property: 'og:type', content: 'website' },
-    { name: 'twitter:card', content: 'summary_large_image' }
+  mode: 'universal',
+  /*
+  ** Nuxt target
+  ** See https://nuxtjs.org/api/configuration-target
+  */
+  target: 'server',
+  /*
+  ** Headers of the page
+  ** See https://nuxtjs.org/api/configuration-head
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { property: 'og:type', content: 'website' },
+      { name: 'twitter:card', content: 'summary_large_image' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href:'https://fonts.googleapis.com/css?family=Lato:300,400,700' }
+    ]
+  },
+  /*
+  ** Enviros
+  */
+  env: {
+    CTF_SPACE_ID: process.env.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
+    CTF_CDA_ACCESS_TOKEN_PREVIEW_API: process.env.CTF_CDA_ACCESS_TOKEN_PREVIEW_API,
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { 
+    color: '#0195ff',
+    height: '6px'
+  },
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@/assets/css/app.css'
   ],
-  link: [
-    { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-    { rel: 'stylesheet', href:'https://fonts.googleapis.com/css?family=Lato:300,400,700' }
-  ]
-},
-
-/*
-** Customize the progress-bar color
-*/
-loading: { 
-  color: '#0195ff',
-  height: '6px'
-},
-
-/*
-** Global CSS
-*/
-css: [
-  '@/assets/css/app.css'
-],
-
   /*
   ** Plugins to load before mounting the App
+  ** https://nuxtjs.org/guide/plugins
   */
   plugins: [
     '@/plugins/globalcomponents.js'
   ],
-
   /*
-  ** Nuxt.js modules
+  ** Auto import components
+  ** See https://nuxtjs.org/api/configuration-components
   */
-  modules: [
-    'nuxt-helmet'
-  ],
-  helmet: {
-    dnsPrefetchControl: true,
-    expectCt: true,
-    featurePolicy: false,
-    frameguard: true,
-    hidePoweredBy: true,
-    hsts: true,
-    ieNoOpen: true,
-    noCache: true,
-    noSniff: true,
-    permittedCrossDomainPolicies: true,
-    referrerPolicy: true,
-    xssFilter: true
-  },
-  /* GTM  */
-  buildModules: [
-    '@nuxtjs/gtm',
-  ],
-  gtm: {
-    id: 'GTM-XXXXXXX'
-  },
-  
+  components: true,
   router: {
     middleware: 'pageTemplate'
   },
-    /*
+  /*
   ** Plugin for 301s and the like
   */
   serverMiddleware: [
     '~/servermiddleware/redirects.js'
-] ,
+  ],
   /*
-  ** Enviros
+  ** Nuxt.js dev-modules
   */
- env: {
-  CTF_SPACE_ID: process.env.CTF_SPACE_ID,
-  CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN,
-  CTF_CDA_ACCESS_TOKEN_PREVIEW_API: process.env.CTF_CDA_ACCESS_TOKEN_PREVIEW_API,
-},
-
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
   /*
   ** Build configuration
+  ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-    }
   }
 }
